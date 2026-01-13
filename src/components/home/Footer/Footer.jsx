238e2db2591800e765acd5d7c82c4935c66b6f83
dxkretoss <dakshn@kretoss.in>
2026-01-13 10:36:43 +0530

    remove phone number from footer bar

--- src/components/home/Footer/Footer.jsx
+++ src/components/home/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center mt-10 md:mt-20">
-      <div className="footerMainDiv px-2 h-[280px] md:h-[450px] xl:h-[450px] 2xl:h-[540px]  relative flex flex-col items-center py-12 gap-5 overflow-hidden">
+      <div className="footerMainDiv px-2 h-[280px] md:h-[420px] xl:h-[420px] 2xl:h-[540px]  relative flex flex-col items-center py-12 gap-5 overflow-hidden">
         <img
           src="./footerTopGradient.png"
           className="w-full absolute top-0 h-[2px] "
         ></img>
-        <h1 className="footerLineText text-black text-lg">
+        {/* <h1 className="footerLineText text-black text-lg">
           <a
             href="tel:+14693875026"
             className="footerLineText text-black text-lg block"
@@ -14,7 +14,7 @@ const Footer = () => {
             +1 (469) 387-5026
           </a>
 
-        </h1>
+        </h1> */}
         <h1 className="footerLineText text-black text-lg">
           kingdomemissary@protonmail.com
         </h1>
